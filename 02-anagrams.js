83288c835b7ef0669567c7d0139a492ff5d91e16
jhoannette arias <jhoannette.arias@gmail.com>
2021-02-24 18:03:41 -0500

    after break

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,3 +1,8 @@
+
+string1 = ("gizmo", "sally");    // => false
+string2 = ("elvis", "lives");    // => true
+
+//log(n)
 function firstAnagram(str1, str2) {
   // Code goes here ....
   // elvis / lives
@@ -6,7 +11,7 @@ function firstAnagram(str1, str2) {
   for (let i = 0; i < arr1.length; i++) {
     let letter = arr1[i];
     let index2 = arr2.indexOf(letter);
-    if (index2 >= 0) {
+    if (index2 >= 0) { //if we had a -1, then we would just never enter the conditional of the second array and perform the splice
       arr2.splice(index2, 1);
     }
   }
@@ -17,11 +22,25 @@ function firstAnagram(str1, str2) {
   }
 }
 
-console.log(firstAnagram("gizmo", "sally"));
+// console.log(firstAnagram("gizmo", "sally"));
 
+//this one is faster than the first one. O(n)
 function secondAnagram(str1, str2) {
   // Code goes here ....
+  // here we needed to make it into a string again in order to strictly compare
+  //these are saved in variables and pointing in different points in memory which is why we need to join
+  let sorted1 = str1.split('').sort().join('')
+  let sorted2 = str2.split('').sort().join('')
+
+  if (sorted1 === sorted2){ //this is comparing by looking directly into spots in memory
+    return true
+  }
+  return false
 }
+
+console.log(secondAnagram("elvis", "lives"))
+
+
 
 function thirdAnagram(str1, str2) {
   // Code goes here ....
