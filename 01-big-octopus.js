3dc82bb7b651a0b4251fd42bc2cbbde2efee48fa
Josh Steinberg <jpsteinberg@gmail.com>
2021-02-24 17:24:26 -0500

    first Anagram finished

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,63 +1,95 @@
-const fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
-'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'];
+const fishies = [
+  "fish",
+  "fiiish",
+  "fiiiiish",
+  "fiiiish",
+  "fffish",
+  "ffiiiiisshh",
+  "fsh",
+  "fiiiissshhhhhh",
+];
 // => "fiiiissshhhhhh";
 
 function quadraticBiggestFish(fishes) {
-//nested loops for n^2
-//outer loop: holds first val of the array
-//inner loop: look at all other values of the array against outer loop val
+  //nested loops for n^2
+  //outer loop: holds first val of the array
+  //inner loop: look at all other values of the array against outer loop val
 
-let longest = ''
+  let longest = "";
 
-  for(let i = 0; i < fishes.length; i++){
-    let currentWord = fishes[i]
+  for (let i = 0; i < fishes.length; i++) {
+    let currentWord = fishes[i];
 
-    for(let j = 0; j < fishes.length; j++){
-      let words = fishes[j]
+    for (let j = 0; j < fishes.length; j++) {
+      let words = fishes[j];
 
-      if(words.length > currentWord.length){
-        longest = words
+      if (words.length > currentWord.length) {
+        longest = words;
       }
     }
   }
-  return longest
+  return longest;
 }
 
 // console.log(quadraticBiggestFish(fishies))
 
-
-function nlognBiggestFish(fishes) { //sorts smallest to largest
+function nlognBiggestFish(fishes) {
+  //sorts smallest to largest
   fishes.sort((a, b) => {
-    a.length - b.length
-  })
-  return fishes[fishes.length - 1] //we want the last in the array
+    a.length - b.length;
+  });
+  return fishes[fishes.length - 1]; //we want the last in the array
 }
 
-console.log(nlognBiggestFish(fishies))
-
-
+// console.log(nlognBiggestFish(fishies))
 
 function linearBiggestFish(fishes) {
   // Code goes here ...
+  let longest = "";
+  for (let i = 0; i < fishes.length; i++) {
+    let word = fishes[i];
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  }
+  return longest;
 }
 
+// console.log(linearBiggestFish(fishies));
 
-tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
+tilesArray = [
+  "up", //0
+  "right-up", //1
+  "right",
+  "right-down",
+  "down",
+  "left-down",
+  "left",
+  "left-up",
+];
 function slowDance(direction, tilesArray) {
   // Code goes here ...
+  for (let i = 0; i < tilesArray.length; i++) {
+    let curDirection = tilesArray[i];
+    if (direction === curDirection) {
+      return i;
+    }
+  }
 }
-
+// console.log(slowDance("right-down", tilesArray));?
 
 tilesObj = {
-  "up": 0,
+  up: 0,
   "right-up": 1,
-  "right": 2,
+  right: 2,
   "right-down": 3,
-  "down": 4,
+  down: 4,
   "left-down": 5,
-  "left": 6,
-  "left-up": 7
-}
+  left: 6,
+  "left-up": 7,
+};
 function fastDance(direction, tilesObj) {
   // Code goes here ...
+  return tilesObj[direction];
 }
+console.log(fastDance("right-down", tilesObj));
--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,18 +1,31 @@
 function firstAnagram(str1, str2) {
   // Code goes here ....
+  // elvis / lives
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+  for (let i = 0; i < arr1.length; i++) {
+    let letter = arr1[i];
+    let index2 = arr2.indexOf(letter);
+    if (index2 >= 0) {
+      arr2.splice(index2, 1);
+    }
+  }
+  if (arr2.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
+console.log(firstAnagram("gizmo", "sally"));
 
 function secondAnagram(str1, str2) {
   // Code goes here ....
 }
 
-
 function thirdAnagram(str1, str2) {
   // Code goes here ....
 }
-
 
 function fourthAnagram(str1, str2) {
   // Code goes here ....
